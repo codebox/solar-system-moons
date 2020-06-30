323d89b8351d82aa7e83ecbc49b05574783a230f
rob <rob@codebox.org.uk>
2020-06-30 12:50:06 +0100

    planet disc maths

--- src/svg.py
+++ src/svg.py
@@ -9,6 +9,9 @@ class Svg:
     def add_circle(self, x, y, r, css_class):
         self.content.append(u'<circle cx="{}" cy="{}" r="{}" class="{}"/>'.format(x, y, r, css_class))
 
+    def add_circle_arc(self, start_x, start_y, end_x, end_y, r, large_angle, sweep, css_class):
+        self.content.append(u'<path d="M {} {} A {} {} 0 {} {} {} {}" class="{}"/>'.format(start_x, start_y, r, r, 1 if large_angle else 0, 1 if sweep else 0, end_x, end_y, css_class))
+
     def add_rectangle(self, x, y, w, h, css_class):
         self.content.append(u'<rect x="{}" y="{}" width="{}" height="{}" class="{}"/>'.format(x, y, w, h, css_class))
 
--- src/svg_wrapper.py
+++ src/svg_wrapper.py
@@ -1,8 +1,11 @@
+import math
+
 HEIGHT=600
 WIDTH=800
 MARGIN=50
 PLANET_BOX_Y_SEPARATION=20
 PLANET_DISC_OVERLAP=50
+
 
 class SvgWrapper:
     def __init__(self, svg):
@@ -27,5 +30,55 @@ class SvgWrapper:
 
         self.svg.save(out_file)
 
-    def _add_planet_box(self, x, y, w, h, planet):
+    def _add_planet_box(self, x, y, w, h, planet_data):
         self.svg.add_rectangle(x, y, w, h, 'planetBox')
+
+        self._add_planet_disc(x, y, h, planet_data['planet']['radius'] * WIDTH)
+
+    def _add_planet_disc(self, x, y, h, radius):
+        if 2 * radius < PLANET_DISC_OVERLAP and 2 * radius < h:
+            '''
+            ┌────
+            │  +
+            │ +++   Whole disc is visible
+            │  +
+            └────
+            '''
+            self.svg.add_circle(x + PLANET_DISC_OVERLAP - radius, y + h/2, radius, 'planetDisc')
+
+        elif 2 * radius >= PLANET_DISC_OVERLAP and 2 * radius < h:
+            '''
+            ┌───
+            │++
+            │+++    Major segment of disc is visible
+            │++
+            └───
+            '''
+            arc_h = math.sqrt(radius * radius - (radius - (2 * radius - PLANET_DISC_OVERLAP)) ** 2)
+            self.svg.add_circle_arc(x, y + h/2 - arc_h, x, y + h/2 + arc_h, radius, True, True, 'planetDisc')
+
+        elif 2 * radius < PLANET_DISC_OVERLAP and 2 * radius >= h:
+            '''
+            ┌───────
+            │  ++++
+            │ ++++++ Top and bottom parts of disc are chopped off
+            │  ++++
+            └───────
+            '''
+            x1 = PLANET_DISC_OVERLAP - (radius - math.sqrt(radius ** 2 - (h**2)/4))
+            x2 = PLANET_DISC_OVERLAP - 2 * radius + math.sqrt(radius ** 2 - (h**2)/4)
+            self.svg.add_circle_arc(x + x1, y, x + x1, y + h, radius, False, True, 'planetDisc')
+            self.svg.add_circle_arc(x + x2, y, x + x2, y + h, radius, False, False, 'planetDisc')
+
+        else:
+            '''
+            ┌──
+            │+
+            │++     Minor segment of disc is visible
+            │+
+            └──
+            '''
+            assert 2 * radius >= PLANET_DISC_OVERLAP and 2 * radius >= h
+            arc_x = PLANET_DISC_OVERLAP - (radius - math.sqrt(radius * radius - h * h / 4))
+            self.svg.add_circle_arc(x + arc_x, y, x + arc_x, y + h, radius, False, True, 'planetDisc')
+
